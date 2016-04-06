b8db3d962f3d4a179131a062036166313c6a050e
Vinit Adkar <vinit.adkar@gmail.com>
2016-04-06 14:11:32 -0700

    Correct typo for bonus to bonusWinner in match schema

--- models/match.js
+++ models/match.js
@@ -46,7 +46,7 @@ var matchSchema = mongoose.Schema({
         rule3Winner: { 
             type: [String],
         },
-        bonus: { 
+        bonusWinner: { 
             type: [String],
         }
     }
