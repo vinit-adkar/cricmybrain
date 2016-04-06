d4d6b399c7085d43a085ba0eab44b97832986f86
Vinit Adkar <vinit.adkar@gmail.com>
2016-04-06 11:59:53 -0700

    Add Schemas for Rule and Match Models Do some renaming for more relevance

---
+++ models/rule.js
@@ -0,0 +1,32 @@
+// app/models/rule.js
+// load the things we need
+var mongoose = require('mongoose');
+
+// define the schema for our rule model
+var ruleSchema = mongoose.Schema({
+
+    local : {
+        ruleNum : { 
+            type: String, 
+            unique: true 
+        },
+        ruleType : { 
+            type: String
+        },
+        ruleDesc : {
+            type: String,
+            unique: true
+        },
+        ruleFunc : {
+            type: String,
+            unique: true
+        }
+        ruleCriteria : { 
+            type: String, 
+        }
+    }
+});
+
+
+// create the model for rules for populating datatbase
+module.exports = mongoose.model('User', ruleSchema);
