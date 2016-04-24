13f8001e5c81a1f9c9068a2a14408cc23aa30535
Anoop Panicker <anoop.vesit@gmail.com>
2016-04-24 09:39:04 -0700

    regex for matching teamname

--- config/passport.js
+++ config/passport.js
@@ -46,7 +46,8 @@ module.exports = function(passport) {
 
             // find a user whose email is the same as the forms email
             // we are checking to see if the user trying to login already exists
-            User.findOne({$or: [{ 'email' :  email.toLowerCase() }, {'teamname' : req.body.teamname.toLowerCase() }]}, function(err, user) {
+            var regexString = '^\\s*' + req.body.teamname + '\\s*$';
+            User.findOne({$or: [{ 'email' :  email.toLowerCase() }, {'teamname' : { $regex : new RegExp(regexString, "i") } }]}, function(err, user) {
                 // if there are any errors, return the error
                 if (err)
                     return done(err);
